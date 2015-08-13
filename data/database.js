65be8d01f3b14a70f51f0aca097eec5b1a704051
Marin Gerov <marin.gerov@gmail.com>
2015-08-13 22:59:06 +0200

    Modified database and started creation of schema

--- data/database.js
+++ data/database.js
@@ -1,30 +1,79 @@
-/**
- *  Copyright (c) 2015, Facebook, Inc.
- *  All rights reserved.
- *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant
- *  of patent rights can be found in the PATENTS file in the same directory.
- */
-
 // Model types
-class User extends Object {}
-class Widget extends Object {}
+export class User extends Object {}
+export class Contact extends Object {}
+
+// Mock authenticated user
+const VIEWER_ID = 'me';
 
 // Mock data
 var viewer = new User();
-viewer.id = '1';
-viewer.name = 'Anonymous';
-var widgets = ['What\'s-it', 'Who\'s-it', 'How\'s-it'].map((name, i) => {
-  var widget = new Widget();
-  widget.name = name;
-  widget.id = `${i}`;
-  return widget;
-});
-
-module.exports = {
-  // Export methods that your schema can use to interact with your database
-  getViewer: () => viewer,
-  getWidget: (id) => widgets.find(w => w.id === id),
-  getWidgets: () => widgets,
+viewer.id = VIEWER_ID;
+var usersById = {
+  [VIEWER_ID]: viewer
 };
+
+// Mock contacts data
+var contactsById = {};
+var contactIdsByUser = {
+  [VIEWER_ID]: []
+};
+var nextContactId = 0;
+
+// Add some secret agents
+var contact1 = {
+  name: 'James Bond',
+  email: 'james.bond@mi5.co.uk',
+  phone: '+1 555 666 333',
+  notes: 'Britain\s top secretagent'
+};
+var contact2 = {
+  name: 'Jason Bourne',
+  email: 'jason@cia.com',
+  phone: '+1 234 567 897',
+  notes: 'Met him during a holiday in Greece'
+};
+var contact3 = {
+  name: 'Ethan Hunt',
+  email: 'ethanhunt@agent.co',
+  phone: '+1 000 111 222',
+  notes: 'Like to hang from buildings'
+};
+addContact(contact1);
+addContact(contact2);
+addContact(contact3);
+
+export function addContact(name, email, phone, notes) {
+  var contact = new Contact();
+  contact.name = name;
+  contact.email = email;
+  contact.phone = phone;
+  contact.notes = notes;
+  contact.id = `${nextContactId++}`;
+  contactsById[contact.id] = contact;
+  contactIdsByUser[VIEWER_ID].push(contact.id);
+  return contact.id;
+}
+
+export function getContact(id) {
+  return contactsById[id];
+}
+
+export function getContacts() {
+  return contactIdsByUser[VIEWER_ID].map((id) => contactsById[id]);
+}
+
+export function getUser(id) {
+  return usersById[VIEWER_ID];
+}
+
+export function getViewer() {
+  return getUser(VIEWER_ID);
+}
+
+export function updateContact(id, name, email, phone, notes) {
+  var contact = getContact(id);
+  contact.name = name;
+  contact.email = email;
+  contact.phone = phone;
+  contact.notes = notes;
+}
--- data/schema.js
+++ data/schema.js
@@ -9,7 +9,6 @@
 
 import {
   GraphQLBoolean,
-  GraphQLFloat,
   GraphQLID,
   GraphQLInt,
   GraphQLList,
@@ -23,19 +22,24 @@ import {
   connectionArgs,
   connectionDefinitions,
   connectionFromArray,
+  cursorForObjectInConnection,
   fromGlobalId,
   globalIdField,
   mutationWithClientMutationId,
   nodeDefinitions,
+  toGlobalId,
 } from 'graphql-relay';
 
 import {
   // Import methods that your schema can use to interact with your database
   User,
-  Widget,
+  Contact,
+  addContact,
+  getContact,
+  getContacts,
+  getUser,
   getViewer,
-  getWidget,
-  getWidgets,
+  updateContact,
 } from './database';
 
 /**
@@ -49,17 +53,17 @@ var {nodeInterface, nodeField} = nodeDefinitions(
     var {type, id} = fromGlobalId(globalId);
     if (type === 'User') {
       return getUser(id);
-    } else if (type === 'Widget') {
-      return getWidget(id);
+    } else if (type === 'Contact') {
+      return getContact(id);
     } else {
       return null;
     }
   },
   (obj) => {
     if (obj instanceof User) {
-      return userType;
-    } else if (obj instanceof Widget)  {
-      return widgetType;
+      return GraphQLUser;
+    } else if (obj instanceof Contact) {
+      return GraphQLContact;
     } else {
       return null;
     }
@@ -70,65 +74,110 @@ var {nodeInterface, nodeField} = nodeDefinitions(
  * Define your own types here
  */
 
-var userType = new GraphQLObjectType({
+var GraphQLUser = new GraphQLObjectType({
   name: 'User',
   description: 'A person who uses our app',
-  fields: () => ({
+  fields: {
     id: globalIdField('User'),
-    widgets: {
-      type: widgetConnection,
-      description: 'A person\'s collection of widgets',
+    contacts: {
+      type: ContactsConnection,
+      description: 'A person\'s collection of contacts',
       args: connectionArgs,
-      resolve: (_, args) => connectionFromArray(getWidgets(), args),
+      resolve: (obj, args) => connectionFromArray(getContacts(), args),
     },
-  }),
+  },
   interfaces: [nodeInterface],
 });
 
-var widgetType = new GraphQLObjectType({
-  name: 'Widget',
-  description: 'A shiny widget',
-  fields: () => ({
-    id: globalIdField('Widget'),
+var GraphQLContact = new GraphQLObjectType({
+  name: 'Contact',
+  description: 'A contact',
+  fields: {
+    id: globalIdField('Contact'),
     name: {
       type: GraphQLString,
-      description: 'The name of the widget',
+      description: 'The name of the contact',
     },
-  }),
+    email: {
+      type: GraphQLString,
+      description: 'The email of the contact',
+    },
+    phone: {
+      type: GraphQLString,
+      description: 'The phone number of the contact',
+    },
+    notes: {
+      type: GraphQLString,
+      description: 'A reminder how you know this contact',
+    },
+  },
   interfaces: [nodeInterface],
 });
 
 /**
  * Define your own connection types here
  */
-var {connectionType: widgetConnection} =
-  connectionDefinitions({name: 'Widget', nodeType: widgetType});
+var {
+  connectionType: ContactsConnection,
+  edgeType: GraphQLContactEdge,
+} = connectionDefinitions({
+  name: 'Contact',
+  nodeType: GraphQLContact,
+  connectionFields: () => ({
+    totalCount: {
+      type: GraphQLInt,
+      resolve: (conn) => conn.edges.length,
+    },
+  })
+});
 
 /**
  * This is the type that will be the root of our query,
  * and the entry point into our schema.
  */
-var queryType = new GraphQLObjectType({
-  name: 'Query',
-  fields: () => ({
-    node: nodeField,
-    // Add your own root fields here
+var Root = new GraphQLObjectType({
+  name: 'Root',
+  fields: {
     viewer: {
-      type: userType,
-      resolve: () => getViewer(),
+      type: GraphQLUser,
+      resolve: () => getViewer()
     },
-  }),
+    node: nodeField,
+  },
 });
 
 /**
  * This is the type that will be the root of our mutations,
  * and the entry point into performing writes in our schema.
  */
-var mutationType = new GraphQLObjectType({
-  name: 'Mutation',
-  fields: () => ({
-    // Add your own mutations here
-  })
+var GraphQLAddContactMutation = mutationWithClientMutationId({
+  name: 'AddContact',
+  inputFields: {
+    name: { name: new GraphQLNonNull(GraphQLString) },
+    email: { email: new GraphQLNonNull(GraphQLString) },
+    phone: { phone: new GraphQLNonNull(GraphQLString) },
+    notes: { notes: new GraphQLNonNull(GraphQLString) },
+  },
+  outputFields: {
+    contactEdge: {
+      type: GraphQLContactEdge,
+      resolve: ({localContactId}) => {
+        var contact = getContact(localContactId);
+        return {
+          cursor: cursorForObjectInConnection(getContacts(), contact),
+          node: contact,
+        };
+      }
+    },
+    viewer: {
+      type: GraphQLUser,
+      resolve: () => getViewer(),
+    },
+  },
+  mutateAndGetPayload: ({name, email, phone, notes}) => {
+    var localContactId = addContact(name, email, phone, notes);
+    return {localContactId};
+  }
 });
 
 /**
