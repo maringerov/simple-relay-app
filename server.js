fee14bc176dadc31336dd4cbca8ceab30c28ebf8
Marin Gerov <marin.gerov@gmail.com>
2015-08-13 23:03:08 +0200

    Changed server port for development

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 import {Schema} from './data/schema';
 
-const APP_PORT = 3000;
+const APP_PORT = 8888;
 const GRAPHQL_PORT = 8080;
 
 // Expose a GraphQL endpoint
