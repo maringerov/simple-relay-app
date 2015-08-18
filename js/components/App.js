ad091a8690cb943dae3b1ffd5653b7b6e434760f
Marin Gerov <marin.gerov@gmail.com>
2015-08-18 16:06:26 +0200

    First App component to read graphql data

--- js/components/App.js
+++ js/components/App.js
@@ -1,7 +1,29 @@
-export default class App extends React.Component {
+import AddContactMutation from '../mutations/AddContactMutation';
+
+class App extends React.Component {
   render() {
     return <div>
-      <h1>Contacts Organizer</h1>
+      <h1>Contact Organizer</h1>
+      <p>Total contacts: {this.props.viewer.contacts.totalCount}</p>
     </div>;
   }
 }
+
+export default Relay.createContainer(App, {
+  fragments: {
+    viewer: () => Relay.QL`
+      fragment on User {
+        contacts(first: 123131) {
+          edges {
+            node {
+              id,
+              name,
+            },
+          },
+          totalCount,
+        },
+        ${AddContactMutation.getFragment('viewer')},
+      }
+    `,
+  },
+});
