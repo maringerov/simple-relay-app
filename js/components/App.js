2c57ec116ff84e18e418e6c56bbd1ad393fe6841
Marin Gerov <marin.gerov@gmail.com>
2015-08-18 15:20:04 +0200

    Configure graphql route and placeholder App component

--- js/components/App.js
+++ js/components/App.js
@@ -1,31 +1,7 @@
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
-    return (
-      <div>
-        <h1>Widget list</h1>
-        <ul>
-          {this.props.viewer.widgets.edges.map(edge =>
-            <li>{edge.node.name} (ID: {edge.node.id})</li>
-          )}
-        </ul>
-      </div>
-    );
+    return <div>
+      <h1>Contacts Organizer</h1>
+    </div>;
   }
 }
-
-export default Relay.createContainer(App, {
-  fragments: {
-    viewer: () => Relay.QL`
-      fragment on User {
-        widgets(first: 10) {
-          edges {
-            node {
-              id,
-              name,
-            },
-          },
-        },
-      }
-    `,
-  },
-});
--- js/routes/AppHomeRoute.js
+++ js/routes/AppHomeRoute.js
@@ -2,7 +2,7 @@ export default class extends Relay.Route {
   static path = '/';
   static queries = {
     viewer: (Component) => Relay.QL`
-      query {
+      query RootQuery{
         viewer {
           ${Component.getFragment('viewer')},
         },
